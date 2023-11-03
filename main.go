a11735df1556b8c5b8a7c3efed1f64a66cfd9ac2
Kalougata <kalougata@111.com>
2023-11-04 07:44:35 +0800

    安装xorm

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
+func main() {
+	app := fiber.New()
+
+	app.Get("", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World!")
+	})
+
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatalf("Failed to listen Serve err: %s \n", err)
+	}
 }
