f13b444710ae5327f9ee2401da8a29dba96993ea
Kalougata <kalougata@111.com>
2023-11-06 06:19:15 +0800

    创建Item模型

--- internal/model/item.go
+++ internal/model/item.go
@@ -7,4 +7,25 @@ type Item struct {
 	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
 	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
 	DeletedAt time.Time `xorm:"deleted DATETIME deleted_at"`
+	Amount    int
+	TagId     int
+	UserId    int
+}
+
+func (i *Item) TableName() string {
+	return "t_items"
+}
+
+type ItemInReq struct {
+	Amount int `json:"amount"`
+	TagId  int `json:"tagId"`
+	UserId int `json:"userId"`
+}
+
+func (u *ItemInReq) ToModel() *Item {
+	return &Item{
+		Amount: u.Amount,
+		TagId:  u.TagId,
+		UserId: u.UserId,
+	}
 }
