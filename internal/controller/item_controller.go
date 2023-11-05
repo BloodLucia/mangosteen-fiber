9da56f98a7796c70f25e6806480ca57eeb8094c0
Kalougata <kalougata@111.com>
2023-11-06 06:25:47 +0800

    ItemService ItemController开发Create方法

--- internal/controller/item_controller.go
+++ internal/controller/item_controller.go
@@ -1,9 +1,19 @@
 package controller
 
-import "github.com/kalougata/bookkeeping/internal/service"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/kalougata/bookkeeping/internal/service"
+	"github.com/kalougata/bookkeeping/pkg/response"
+)
 
 type ItemController struct {
 	service *service.ItemService
+}
+
+func (ic *ItemController) Create() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		return response.Handle(ctx, nil, "create item")
+	}
 }
 
 func NewItemController(service *service.ItemService) *ItemController {
--- internal/service/item_service.go
+++ internal/service/item_service.go
@@ -1,9 +1,17 @@
 package service
 
-import "github.com/kalougata/bookkeeping/internal/data"
+import (
+	"context"
+	"github.com/kalougata/bookkeeping/internal/data"
+	"github.com/kalougata/bookkeeping/internal/model"
+)
 
 type ItemService struct {
 	data *data.Data
+}
+
+func (is *ItemService) Create(ctx context.Context, req *model.ItemInReq) (int64, error) {
+	return is.data.DB.Context(ctx).Table(&model.Item{}).Insert(req.ToModel())
 }
 
 func NewItemService(data *data.Data) *ItemService {
