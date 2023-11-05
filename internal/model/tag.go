833c48cfc2a3fe5bd9b462b397cb6a4d5db72d2e
Kalougata <kalougata@111.com>
2023-11-05 15:14:21 +0800

    创建Tag模型

---
+++ internal/model/tag.go
@@ -0,0 +1,31 @@
+package model
+
+import "time"
+
+type Tag struct {
+	ID        uint64    `xorm:"not null pk autoincr BIGINT(20) id"`
+	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
+	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
+	DeletedAt time.Time `xorm:"deleted DATETIME deleted_at"`
+	Name      string
+	Type      string
+	Sign      string
+}
+
+func (u *Tag) TableName() string {
+	return "t_tags"
+}
+
+type TagInReq struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Sign string `json:"sign"`
+}
+
+func (u *TagInReq) ToModel() *Tag {
+	return &Tag{
+		Name: u.Name,
+		Type: u.Type,
+		Sign: u.Sign,
+	}
+}
