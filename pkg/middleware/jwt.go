3c08a2b9297129eaa86e50cdd29b91e64c6b5c01
Kalougata <kalougata@111.com>
2023-11-06 21:50:15 +0800

    自定义jwt中间件

---
+++ pkg/middleware/jwt.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/gookit/goutil"
+	"github.com/kalougata/bookkeeping/pkg/e"
+	myJwt "github.com/kalougata/bookkeeping/pkg/jwt"
+	"github.com/kalougata/bookkeeping/pkg/response"
+)
+
+type JWTMiddleware struct {
+	jwt *myJwt.JWT
+}
+
+func (jm *JWTMiddleware) JWTAuth() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		tokenString := ctx.Get("Authorization")
+		if goutil.IsEqual(tokenString, "") {
+			return response.Handle(ctx, e.ErrUnauthorized(), nil)
+		}
+		claims, err := jm.jwt.ParseToken(tokenString)
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenExpired) {
+				return response.Handle(ctx, e.ErrUnauthorized().WithMsg("token已过期, 清重新登录"), nil)
+			}
+			return response.Handle(ctx, e.ErrUnauthorized(), nil)
+		}
+		ctx.Set("userId", goutil.String(claims.UserId))
+
+		return ctx.Next()
+	}
+}
