454e3fb3a2033646df874eac08ccd6d4728f2a86
Kalougata <kalougata@111.com>
2023-11-17 10:16:59 +0800

    [✍️]Tag模型添加json标签

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Tag struct {
-	ID        uint64    `xorm:"not null pk autoincr BIGINT(20) id"`
-	CreatedAt time.Time `xorm:"created TIMESTAMP created_at"`
-	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at"`
-	Name      string    `xorm:"not null VARCHAR(30) name"`
-	Kind      string    `xorm:"not null VARCHAR(10) kind"`
-	Sign      string    `xorm:"not null CHAR(1) sign"`
-	UserId    uint64    `xorm:"not null BIGINT(20) user_id"`
+	ID        uint64    `xorm:"not null pk autoincr BIGINT(20) id" json:"id"`
+	Name      string    `xorm:"not null VARCHAR(30) name" json:"name"`
+	Kind      string    `xorm:"not null VARCHAR(10) kind" json:"kind"`
+	Sign      string    `xorm:"not null CHAR(1) sign" json:"sign"`
+	UserId    uint64    `xorm:"not null BIGINT(20) user_id" json:"user_id"`
+	CreatedAt time.Time `xorm:"created TIMESTAMP created_at" json:"created_at"`
+	UpdatedAt time.Time `xorm:"updated TIMESTAMP updated_at" json:"updated_at"`
 }
 
-func (u *Tag) TableName() string {
+func (t *Tag) TableName() string {
 	return "t_tags"
 }
